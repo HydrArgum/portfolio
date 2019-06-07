090a8789c73eb003b625f15fe435480449929c25
Kristóf Nagy <hydrargum@gmail.com>
2019-06-07 15:52:58 +0200

    modif content

--- golya_3/src/logic/Beka.java
+++
@@ -1,9 +0,0 @@
-package logic;
-
-public class Beka extends Allat {
-
-    public Beka() {
-        this.faj = fajEnum.B;
-    }
-
-}
--- golya_3/src/logic/fajEnum.java
+++
@@ -1,6 +0,0 @@
-package logic;
-public enum fajEnum {
-    G, //Golya
-    B, //Béka
-    L  //Légy
-}
